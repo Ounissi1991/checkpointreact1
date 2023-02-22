bc8c2a90a24db848af20bb2e601693343f2ebe01
Oussama <wessou.ounissi@gmail.com>
2023-02-22 21:35:31 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+    <h1>Welcome to my first ReactJs Project</h1>
+    <h2>Ounissi Oussama</h2>
+        <p>
+         I am trying to deal with new skills in wishing of getting to improve my career 
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/Ounissi1991?tab=repositories"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Here is My github repositries
+        </a>
+        <div class="container">
+          <h3>see you ! </h3>
+            </div>
+      </header>
+      
+
+<select class="form-select form-select-sm" aria-label=".form-select-sm example">
+  <option selected>Open this select menu</option>
+  <option value="1">One</option>
+  <option value="2">Two</option>
+  <option value="3">Three</option>
+</select>
+     
+
+      <footer className="App-footer">
+      <div class="container-fluid">
+      ...
+      </div>
+
+      </footer>
+    </div>
+  );
+}
+
+export default App;
